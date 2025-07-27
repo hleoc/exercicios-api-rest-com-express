b37c53a2db41e74ed2c8dbd533b3b8cf3d20128e
Hugo Costa <informig@gmail.com>
2025-07-27 15:09:53 -0300

    Exercício 5

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { readMoviesData } = require('./utils/fsUtils');
+const { readMoviesData, readMoviesDataId } = require('./utils/fsUtils');
 
 const app = express();
 
@@ -7,7 +7,13 @@ app.use(express.json());
 
 app.get('/movies', async (req, res) => {
     const readMovies = await readMoviesData();
-    res.status(200).json({ movies: readMovies });
+    return res.status(200).json({ movies: readMovies });
+});
+
+app.get('/movies/:id', async (req, res) => {
+    const { id } = req.params;
+    const readMoviesId = await readMoviesDataId(Number(id));
+    return res.status(200).json({ movie: readMoviesId });
 });
 
 module.exports = app;
